c19197f583e5cb2b30db41a8a39cbe80eb2176cd
Ronald Mera <roncollege1026@gmail.com>
2022-01-26 09:46:44 -0500

    Header

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAe6Sm3i3i2fcA6wVHC-BTSbXbJiv4WJj8",
+  authDomain: "challenge-64ccf.firebaseapp.com",
+  projectId: "challenge-64ccf",
+  storageBucket: "challenge-64ccf.appspot.com",
+  messagingSenderId: "2121536422",
+  appId: "1:2121536422:web:96d1a1333b5f98d95ab567",
+  measurementId: "G-Y73QT7WQM9"
+};
