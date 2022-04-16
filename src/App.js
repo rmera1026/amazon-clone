f4d2e4af8debce8f97a18b76a90d5c9a3f00cc45
Ronald Mera <roncollege1026@gmail.com>
2022-04-16 19:30:17 -0400

    Front page complete

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import Header from './Header';
+import Home from './Home';
 
 function App() {
   return (
     <div className="App">
       {/* header */}
       <Header></Header>
+      <Home></Home>
       {/* home */}
+
     </div>
   );
 }
